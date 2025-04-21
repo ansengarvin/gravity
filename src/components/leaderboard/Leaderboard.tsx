667ef0a56b143e575c906044b617f54a72d8ab85
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2025-04-20 19:34:11 -0700

    feat: additional styling changes for following bodies

--- src/components/leaderboard/Leaderboard.tsx
+++ src/components/leaderboard/Leaderboard.tsx
@@ -8,16 +8,11 @@ import { LeaderboardBody } from "./LeaderboardBody.tsx";
 interface LeaderboardProps {
     leaderboardBodies: LeaderboardBody[];
     bodyFollowed: number;
-    updateBodyFollowed: (newBodiesFollowed: number) => void;
+    updateBodyFollowed: (newBodyFollowed: number) => void;
 }
 
 export function Leaderboard(props: LeaderboardProps) {
     const { leaderboardBodies, bodyFollowed, updateBodyFollowed } = props;
-
-    const cardClick = (idx: number, x: number, y: number, z: number) => {
-        console.log(x, y, z);
-        updateBodyFollowed(idx);
-    };
 
     return (
         <LeaderboardStyle>
@@ -26,7 +21,8 @@ export function Leaderboard(props: LeaderboardProps) {
                 <LeaderboardItemCard
                     key={index}
                     item={item}
-                    selected={bodyFollowed === item.index}
+                    followed={bodyFollowed === item.index}
+                    updateBodyFollowed={updateBodyFollowed}
                 />
             ))}
         </LeaderboardStyle> 
@@ -43,7 +39,7 @@ const LeaderboardStyle = styled.div`
     height: 200px;
     width: min-content;
     overflow-y: scroll;
-    background-color: #050505;
+    background-color: black;
     margin-left: auto;
     margin-right: auto;
     border: 4px solid white;
--- src/components/leaderboard/LeaderboardItemCard.tsx
+++ src/components/leaderboard/LeaderboardItemCard.tsx
@@ -1,25 +1,97 @@
 import styled from "@emotion/styled";
 import { brightenColor } from "../../lib/colors/brightenColor";
 import { LeaderboardBody } from "./LeaderboardBody";
+import { TargetIcon } from "../../assets/icons/TargetIcon";
 
-const LeaderboardItemCardStyle = styled.div<{color: string}>`
+interface LeaderboardItemCardProps {
+    item: LeaderboardBody;
+    followed: boolean;
+    updateBodyFollowed: (newBodyFollowed: number) => void;
+}
+
+const ButtonWidth = '50px';
+const NameWidth = '50px';
+const MassWidth = '100px';
+const DistanceWidth = '120px';
+
+
+export function LeaderboardHeader() {
+    return (
+        <LeaderboardHeaderStyle color={'black'} followed={false}>
+            <HeaderCard width={ButtonWidth}>
+               
+            </HeaderCard>
+            <HeaderCard width={NameWidth}>
+                Name
+            </HeaderCard>
+            <HeaderCard width={MassWidth}>
+                Mass (M<sub>☉</sub>)
+            </HeaderCard>
+            <HeaderCard width={DistanceWidth}>
+                Dist. (Origin, AU)
+            </HeaderCard>
+            <HeaderCard width={DistanceWidth}>
+                Dist. (Target, AU)
+            </HeaderCard>
+        </LeaderboardHeaderStyle>
+    )
+}
+
+export function LeaderboardItemCard(props: LeaderboardItemCardProps) {
+    const { item, followed, updateBodyFollowed} = props;
+
+    return (
+        <LeaderboardItemCardStyle color={item.color} followed={followed}>
+            <SelectButton color={item.color} onClick={() => {
+                updateBodyFollowed(item.index);
+            }}>
+                <TargetIcon filled={true} color={item.color} dim={'35px'}/>
+            </SelectButton>
+            <InfoCard width={NameWidth}>
+                B-{item.index.toFixed()}
+            </InfoCard>
+            <InfoCard width={MassWidth}>
+                {item.mass.toFixed(2)}
+            </InfoCard>
+            <InfoCard width={DistanceWidth}>
+                {
+                    Math.sqrt(item.pos.x**2 + item.pos.y**2 + item.pos.z**2).toFixed(2)
+                }
+            </InfoCard>
+            <InfoCard width={DistanceWidth}>
+                --
+            </InfoCard>
+        </LeaderboardItemCardStyle>
+    )
+}
+
+const LeaderboardItemCardStyle = styled.div<{color: string, followed: boolean}>`
     display: flex;
     flex-direction: row;
     gap: 5px;
+    margin-top: 5px;
     width: min-content;
     height: min-content;
+
+
+    border-top: ${props => props.followed ? '3px solid red' : 'none'};
+    border-bottom: ${props => props.followed ? '3px solid red' : 'none'};
 
     margin-bottom: 5px;
 
     & > * {
         background-color: ${props => props.color};
     };
-    &:hover >* {
+
+    button {
+        background-color: black;
+    }
+    /* &:hover >* {
         background-color: ${props => brightenColor(props.color, 1.3)};
     }
     button: {
         background-color: ${props => brightenColor(props.color, 1.7)};
-    }
+    } */
 `;
 
 const InfoCard = styled.div<{width: string}>`
@@ -53,69 +125,15 @@ const HeaderCard = styled(InfoCard)`
     }
 `
 
-const SelectButton = styled.button`
+const SelectButton = styled.button<{color: string}>`
+    display: flex;
+    justify-content: center;
+    align-items: center;
     width: 50px;
 
     // Remove all styling
     border: none;
     padding: none;
+
+    //background-color: black;
 `
-
-interface LeaderboardItemCardProps {
-    item: LeaderboardBody;
-    selected: boolean;
-}
-
-const ButtonWidth = '50px';
-const NameWidth = '50px';
-const MassWidth = '100px';
-const DistanceWidth = '120px';
-
-
-export function LeaderboardHeader() {
-    return (
-        <LeaderboardHeaderStyle color={'black'}>
-            <HeaderCard width={ButtonWidth}>
-               
-            </HeaderCard>
-            <HeaderCard width={NameWidth}>
-                Name
-            </HeaderCard>
-            <HeaderCard width={MassWidth}>
-                Mass (M<sub>☉</sub>)
-            </HeaderCard>
-            <HeaderCard width={DistanceWidth}>
-                Dist. (Origin, AU)
-            </HeaderCard>
-            <HeaderCard width={DistanceWidth}>
-                Dist. (Target, AU)
-            </HeaderCard>
-        </LeaderboardHeaderStyle>
-    )
-}
-
-export function LeaderboardItemCard(props: LeaderboardItemCardProps) {
-    const { item, selected } = props;
-
-    return (
-        <LeaderboardItemCardStyle color={item.color}>
-            <SelectButton>
-                
-            </SelectButton>
-            <InfoCard width={NameWidth}>
-                {item.index.toFixed()}
-            </InfoCard>
-            <InfoCard width={MassWidth}>
-                {item.mass.toFixed(2)}
-            </InfoCard>
-            <InfoCard width={DistanceWidth}>
-                {
-                    Math.sqrt(item.pos.x**2 + item.pos.y**2 + item.pos.z**2).toFixed(2)
-                }
-            </InfoCard>
-            <InfoCard width={DistanceWidth}>
-                --
-            </InfoCard>
-        </LeaderboardItemCardStyle>
-    )
-}
