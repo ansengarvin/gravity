706991ea2cb4ac6808a0aba9a3e54ffab208d086
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2025-05-01 14:44:23 -0700

    test linting

---
+++ src/components/TestLint.tsx
@@ -0,0 +1,8 @@
+export function TestComponent() {
+    return (
+        <div>
+        <h1>Test Component</h1>
+        <p>This is a test component. Its only purpose is to test linting.</p>
+        </div>
+    );
+}
