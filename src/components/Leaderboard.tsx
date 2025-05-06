54e2a445747ad08d75448f46c085c5c96c2fa23e
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2025-05-05 19:01:17 -0700

    feat: added orbital tab

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -2,6 +2,11 @@ import styled from "@emotion/styled";
 import { useMemo, useState } from "react";
 import { brightenColor } from "../lib/colors/brightenColor";
 
+enum TabType {
+    BASIC = "basic",
+    ORBIT = "orbit",
+}
+
 export interface LeaderboardBody {
     index: number;
     mass: number;
@@ -25,75 +30,189 @@ export function Leaderboard(props: LeaderboardProps) {
     const sortedBodies = useMemo(() => {
         return sortBodies(leaderboardBodies, sortBy);
     }, [sortBy, leaderboardBodies, bodyFollowed]);
-    return (
-        <LeaderboardStyle>
-            <table>
-                <thead>
-                <tr>
-                    <th>
-                        <button onClick={() => {
-                            sortBy == "name" ? setSortBy("nameReverse") : setSortBy("name");
-                        }}>
-                            Name
-                        </button>
-                    </th>
-                    <th>
-                        <button onClick={() => {
-                            sortBy == "mass" ? setSortBy("massReverse") : setSortBy("mass");
-                        }}>
-                            Mass
-                        </button>
-                    </th>
-                    <th>
-                        <button onClick={() => {
-                            sortBy == "dOrigin" ? setSortBy("dOriginReverse") : setSortBy("dOrigin");
-                        }}>
-                            dOrigin
-                        </button>
-                    </th>
-                    <th>
-                        <button onClick={() => {
-                            sortBy == "dTarget" ? setSortBy("dTargetReverse") : setSortBy("dTarget");
-                        }}>
-                            dTarget
-                        </button>
-                    </th>
-                </tr>
-                </thead>
-                <tbody>
-                    {sortedBodies.map((body: LeaderboardBody) => {
-                        return (
-                            <LeaderboardRowStyle
-                                key={body.index}
-                                bodyColor={body.color}
-                                selected={bodyFollowed == body.index}
-                            >
-                                <td>
-                                    <button
-                                        onClick={() => {
-                                            setBodyFollowed(body.index);
-                                        }}
-                                        disabled={bodyFollowed == body.index}
-                                    >
-                                        B-{body.index}
-                                    </button>
-                                </td>
-                                <td>{body.mass.toFixed(2)}</td>
-                                <td>{body.dOrigin.toFixed(2)}</td>
-                                <td>{bodyFollowed != -1 ? body.dTarget.toFixed(2) : "--"}</td>
-                            </LeaderboardRowStyle>
-                        );
-                    })}
-                </tbody>
-            </table>
-        </LeaderboardStyle>
-    );
+
+    const [leaderboardTab, setLeaderboardTab] = useState<TabType>(TabType.BASIC);
+
+    switch (leaderboardTab) {
+        case TabType.BASIC:
+            return (
+                <LeaderboardStyle>
+                    <LeaderboardTabs leaderboardTab={leaderboardTab} setLeaderboardTab={setLeaderboardTab} />
+                    <LeaderboardContent>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>
+                                        <button
+                                            onClick={() => {
+                                                sortBy == "name" ? setSortBy("nameReverse") : setSortBy("name");
+                                            }}
+                                        >
+                                            Name
+                                        </button>
+                                    </th>
+                                    <th>
+                                        <button
+                                            onClick={() => {
+                                                sortBy == "mass" ? setSortBy("massReverse") : setSortBy("mass");
+                                            }}
+                                        >
+                                            Mass
+                                        </button>
+                                    </th>
+                                    <th>
+                                        <button
+                                            onClick={() => {
+                                                sortBy == "dOrigin"
+                                                    ? setSortBy("dOriginReverse")
+                                                    : setSortBy("dOrigin");
+                                            }}
+                                        >
+                                            dOrigin
+                                        </button>
+                                    </th>
+                                    <th>
+                                        <button
+                                            onClick={() => {
+                                                sortBy == "dTarget"
+                                                    ? setSortBy("dTargetReverse")
+                                                    : setSortBy("dTarget");
+                                            }}
+                                        >
+                                            dTarget
+                                        </button>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {sortedBodies.map((body: LeaderboardBody) => {
+                                    return (
+                                        <LeaderboardRowStyle
+                                            key={body.index}
+                                            bodyColor={body.color}
+                                            selected={bodyFollowed == body.index}
+                                        >
+                                            <td>
+                                                <BodySelectButton
+                                                    selected={bodyFollowed == body.index}
+                                                    bodyColor={body.color}
+                                                    onClick={() => {
+                                                        setBodyFollowed(body.index);
+                                                    }}
+                                                    disabled={bodyFollowed == body.index}
+                                                >
+                                                    B-{body.index}
+                                                </BodySelectButton>
+                                            </td>
+                                            <td>{body.mass.toFixed(2)}</td>
+                                            <td>{body.dOrigin.toFixed(2)}</td>
+                                            <td>{bodyFollowed != -1 ? body.dTarget.toFixed(2) : "--"}</td>
+                                        </LeaderboardRowStyle>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
+                    </LeaderboardContent>
+                </LeaderboardStyle>
+            );
+        case TabType.ORBIT:
+            return (
+                <LeaderboardStyle>
+                    <LeaderboardTabs leaderboardTab={leaderboardTab} setLeaderboardTab={setLeaderboardTab} />
+                    <LeaderboardContent>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>
+                                        <button
+                                            onClick={() => {
+                                                sortBy == "name" ? setSortBy("nameReverse") : setSortBy("name");
+                                            }}
+                                        >
+                                            Name
+                                        </button>
+                                    </th>
+                                    <th>
+                                        <button
+                                            onClick={() => {
+                                                sortBy == "mass" ? setSortBy("massReverse") : setSortBy("mass");
+                                            }}
+                                        >
+                                            Mass
+                                        </button>
+                                    </th>
+                                    <th>
+                                        <button
+                                            onClick={() => {
+                                                sortBy == "orbiting"
+                                                    ? setSortBy("orbitingReverse")
+                                                    : setSortBy("orbiting");
+                                            }}
+                                        >
+                                            Orbiting
+                                        </button>
+                                    </th>
+                                    <th>
+                                        <button
+                                            onClick={() => {
+                                                sortBy == "dOrbit" ? setSortBy("dOrbitReverse") : setSortBy("dOrbit");
+                                            }}
+                                        >
+                                            dOrbit
+                                        </button>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {sortedBodies.map((body: LeaderboardBody) => {
+                                    return (
+                                        <LeaderboardRowStyle
+                                            key={body.index}
+                                            bodyColor={body.color}
+                                            selected={bodyFollowed == body.index}
+                                        >
+                                            <td>
+                                                <BodySelectButton
+                                                    selected={bodyFollowed == body.index}
+                                                    bodyColor={body.color}
+                                                    onClick={() => {
+                                                        setBodyFollowed(body.index);
+                                                    }}
+                                                    disabled={bodyFollowed == body.index}
+                                                >
+                                                    B-{body.index}
+                                                </BodySelectButton>
+                                            </td>
+                                            <td>{body.mass.toFixed(2)}</td>
+                                            <td>
+                                                <BodySelectButton
+                                                    selected={bodyFollowed == body.index}
+                                                    bodyColor={body.orbitColor}
+                                                    onClick={() => {
+                                                        setBodyFollowed(body.index);
+                                                    }}
+                                                    disabled={bodyFollowed == body.index}
+                                                >
+                                                    B-{body.index}
+                                                </BodySelectButton>
+                                            </td>
+                                            <td>{body.dOrbit.toFixed(2)}</td>
+                                        </LeaderboardRowStyle>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
+                    </LeaderboardContent>
+                </LeaderboardStyle>
+            );
+        default:
+            <></>;
+    }
 }
 
 /*
     Styled Components
 */
-
 const LeaderboardStyle = styled.div`
     grid-area: menus;
     margin-left: auto;
@@ -102,12 +221,16 @@ const LeaderboardStyle = styled.div`
     height: 100%;
 
     background-color: black;
+`;
+
+const LeaderboardContent = styled.div`
     overflow-y: auto;
+    height: 85%;
 
     table {
+        height: 100%;
         width: 100%;
         border-spacing: 5px;
-
     }
 
     th {
@@ -130,8 +253,6 @@ const LeaderboardStyle = styled.div`
             cursor: pointer;
         }
     }
-
-
 `;
 
 const LeaderboardRowStyle = styled.tr<{ bodyColor: string; selected: boolean }>`
@@ -144,29 +265,79 @@ const LeaderboardRowStyle = styled.tr<{ bodyColor: string; selected: boolean }>`
 
         border: ${(props) => (props.selected ? "3px solid " + props.bodyColor : "none")};
 
-        button {
-            // Remove all styling
-            background: none;
-            border: none;
-            color: inherit;
-            font: inherit;
-            cursor: pointer;
-            outline: inherit;
-            width: 100%;
-            height: 100%;
+        color: black;
+    }
+`;
 
-            border: 3px solid ${(props) => (props.selected ? "black" : brightenColor(props.bodyColor, 0.5))};
+const BodySelectButton = styled.button<{ selected: boolean; bodyColor: string }>`
+    // Remove all styling
+    background: none;
+    border: none;
+    color: inherit;
+    font: inherit;
+    cursor: pointer;
+    outline: inherit;
+    width: 100%;
+    height: 100%;
 
-            :hover {
-                background-color: ${(props) => brightenColor(props.bodyColor, 1.2)};
-            }
+    background-color: ${(props) => (props.selected ? "black" : props.bodyColor)};
 
-            :disabled {
-                background-color: black;
-                color: ${(props) => props.bodyColor};
-                cursor: auto;
-            }
-        }
+    border: 3px solid ${(props) => (props.selected ? "black" : brightenColor(props.bodyColor, 0.5))};
+
+    :hover {
+        background-color: ${(props) => brightenColor(props.bodyColor, 1.2)};
+    }
+
+    :disabled {
+        background-color: black;
+        color: ${(props) => props.bodyColor};
+        cursor: auto;
+    }
+`;
+
+interface LeaderboardTabsProps {
+    leaderboardTab: TabType;
+    setLeaderboardTab: React.Dispatch<React.SetStateAction<TabType>>;
+}
+function LeaderboardTabs(props: LeaderboardTabsProps) {
+    const { leaderboardTab, setLeaderboardTab } = props;
+    return (
+        <LeaderboardTabsStyle>
+            <button
+                onClick={() => {
+                    setLeaderboardTab(TabType.BASIC);
+                }}
+            >
+                Properties
+            </button>
+            <button
+                onClick={() => {
+                    setLeaderboardTab(TabType.ORBIT);
+                }}
+            >
+                Orbits
+            </button>
+        </LeaderboardTabsStyle>
+    );
+}
+
+const LeaderboardTabsStyle = styled.div`
+    height: 15%;
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: left;
+    align-items: center;
+    gap: 1px;
+
+    button {
+        height: 100%;
+        background: none;
+        border: none;
+        background-color: #faff6f;
+        cursor: pointer;
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
     }
 `;
 
