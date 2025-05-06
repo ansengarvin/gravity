faeb80c786ff109361e5441b277b6285b3afc667
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2025-05-05 17:44:41 -0700

    chore: removed unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import styled from "@emotion/styled";
 import { Sim } from "./components/Sim";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import { Header } from "./components/Header";
 import { ControlButtons } from "./components/ControlButtons";
 import { SettingsMenu } from "./components/Settings";
