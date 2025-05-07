125e23a4ab1fbc1b22897ec220732f69e1226247
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2025-05-07 13:43:46 -0700

    fix: minimum rgb value of planetary bodies raised to resolve contrast issues

--- src/lib/universe/universe.tsx
+++ src/lib/universe/universe.tsx
@@ -123,9 +123,9 @@ export class Universe {
 
         // Set colors
         for (let i = 0; i < this.settings.numBodies; i++) {
-            this.colorsR[i] = getRandomFloat(0.1, 0.85);
-            this.colorsG[i] = getRandomFloat(0.1, 0.85);
-            this.colorsB[i] = getRandomFloat(0.1, 0.85);
+            this.colorsR[i] = getRandomFloat(0.2, 0.85);
+            this.colorsG[i] = getRandomFloat(0.2, 0.85);
+            this.colorsB[i] = getRandomFloat(0.2, 0.85);
         }
 
         this.setOrbitalInformation();
