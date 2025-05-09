a1d7266c40437980c2ea23ed72208d5bdbaffc4a
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2025-05-08 23:02:28 -0700

    feat(wip): added star color extraction to second location for bloom

--- src/assets/shaders/starlight/starlight.frag.glsl
+++ src/assets/shaders/starlight/starlight.frag.glsl
@@ -17,7 +17,8 @@ uniform highp vec3 uStarLocations[MAX_STARS];
 //     highp vec3 uboStarLocations[MAX_STARS];
 // };
 
-out highp vec4 fragColor;
+layout(location=0) out highp vec4 fragColor;
+layout(location=1) out highp vec4 brightColor;
 
 void main(void) {
     highp vec3 ambient = vec3(0.05, 0.05, 0.05);
@@ -49,34 +50,29 @@ void main(void) {
         
         diffuse += diff * uFragColor.rgb * attenuation_factor;
     }
-    
-    /*
-    // Light calculated only from single-most-massive star
-    if (uNumStars > 0) {
-
-        // Calculate the direction to the largest star
-        directionalVector = normalize(uStarLocations[0] - vFragPosition);
-        highp float diff = max(dot(normal, directionalVector), 0.0);
-
-        // Calculate the distance
-        highp float dist = distance(uStarLocations[0], vFragPosition);
-
-        // Light should linearly attenuate from 100% to 0%, falling off to 0 at 50au
-        // According to graphing calculator, y = -x/50 + 1 is the formula
-        // (This has no basis in physics, but may produce decent-looking results)
-        highp float attenuation_factor = ((-1.0 * dist) / 50.0) + 1.0;
-        
-        diffuse += diff * uFragColor.rgb * attenuation_factor;
-    }
-    */
 
     if (uIsStar > 0) {
         ambient = vec3(1.5, 1.5, 1.5);
         diffuse = vec3(0.0, 0.0, 0.0);
     }
 
-
-
     highp vec3 result = (ambient + diffuse) * uFragColor.rgb;
     fragColor = vec4(result, uFragColor.a);
+
+    // THis is normally how bloom would be handled - based on the brightness of all fragments.
+    /*
+    float brightness = dot(FragColor.rgb, vec3(0.2126, 0.7152, 0.0722));
+    if(brightness > 1.0) {
+        BrightColor = vec4(FragColor.rgb, 1.0);
+    } else {
+        BrightColor = vec4(0.0, 0.0, 0.0, 1.0);
+    }
+    */
+
+    // My approach will be to just extract the light from stars and add a bloom effect therein.
+    if (uIsStar > 0) {
+        brightColor = fragColor;
+    } else {
+        brightColor = vec4(0.0, 0.0, 0.0, 1.0);
+    }
 }
