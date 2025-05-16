b78d7893910f3a4db9d9b950aaff84290d379554
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2025-05-15 18:38:31 -0700

    feat: bloom merge color tweaks

--- src/assets/shaders/bloom/bloom.frag.glsl
+++ src/assets/shaders/bloom/bloom.frag.glsl
@@ -13,11 +13,14 @@ uniform highp sampler2D uScene;
 uniform highp sampler2D uBloom;
 
 void main() {
-    const highp float gamma = 1.0;
+    const highp float gamma = 1.25;
     const highp float exposure = 1.0;
 
     highp vec3 sceneColor = texture(uScene, TexCoords).rgb;
     highp vec3 bloomColor = texture(uBloom, TexCoords).rgb;
+
+    // Adding an additional layer of gamma to bloomcolor makes for a nice looking result
+    bloomColor = pow(bloomColor, vec3(1.0 / 1.25));
 
     highp vec3 color = sceneColor + bloomColor;
 
