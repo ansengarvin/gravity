12bf48f207d1b8ae667ad5eb15ad4e098c889b13
ansengarvin <45224464+ansengarvin@users.noreply.github.com>
2025-05-17 11:11:00 -0700

    feat: update and display fps and tps

--- src/components/DebugStats.tsx
+++ src/components/DebugStats.tsx
@@ -7,6 +7,8 @@ export function DebugStats() {
 
     return (
         <DebugStatsStyle>
+            <div>FPS: {debug.fps}</div>
+            <div>TPS: {debug.tps}</div>
             <h2>Debug Stats</h2>
             <h3>Simulation</h3>
             <div>Number of Bodies: {debug.numActiveBodies}</div>
